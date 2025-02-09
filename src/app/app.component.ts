eda323dac1fcb7cda76e7dc54422fc2f148455ec
AmitJ <amitjgtp2@gmail.com>
2025-02-09 10:48:28 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarvellousService } from './marvellous.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone:false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Client';
+  message:any;
+
+  constructor(private service:MarvellousService)
+  {}
+
+  ngOnInit(): void {
+      this.service.getBatches().subscribe(data=>{
+        this.message = data;
+      })
+  }
 }
